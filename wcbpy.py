a532369f9c7b9a809435ad48e38f8e93b33752e7
jrheard <jrheard@cs.stanford.edu>
2017-09-20 17:43:09 -0700

    initial commit

---
+++ wcbpy.py
@@ -0,0 +1,97 @@
+import requests
+import turtle
+
+### Private, implementation details
+
+# Global mutable state. Forgive me.
+state = {
+    'connected_to_bot': False,
+}
+
+def make_cnc_request(endpoint):
+    requests.get("http://localhost:4242/" + endpoint)
+
+# Public
+
+def initialize():
+    brush_up()
+    park()
+    # reset turtle
+    # TODO how to deal with scratch positions vs turtle positions? make a utility conversion function
+
+def cleanup():
+    brush_up()
+    wash_brush()
+    park()
+
+def park():
+    make_cnc_request("park")
+
+def wash_brush():
+    make_cnc_request("pen.wash")
+
+def get_color(index):
+    if index in range(0, 8):
+        make_cnc_request("tool.color./" + str(index))
+    else:
+        print("Color indexes must be between 0 and 7, but you gave me: " + index)
+
+def brush_down():
+    make_cnc_request("pen.down")
+
+def brush_up():
+    make_cnc_request("pen.up")
+
+def move_to(x, y):
+    make_cnc_request("coord/{0}/{1}".format(x, y))
+
+def point_in_direction(angle):
+    make_cnc_request("move.absturn./" + str(angle))
+
+def move_forward(num_steps):
+    make_cnc_request("move.forward./" + str(num_steps))
+
+def turn_left(relative_angle):
+    make_cnc_request("move.left./" + str(relative_angle))
+
+def turn_right(relative_angle):
+    make_cnc_request("move.right./" + str(relative_angle))
+
+# Test program
+
+
+def flower_scene():
+    initialize()
+    wash_brush()
+    get_color(4) # green
+
+    move_to(-100, -145)
+    point_in_direction(20)
+
+    # stem
+    brush_down()
+    for _ in range(25):
+        move_forward(5)
+        turn_left(1)
+    for _ in range(20):
+        move_forward(5)
+        turn_right(1)
+
+
+
+
+
+
+     # TODO record current x, y position - get them from turtle?
+     # ugh shit
+
+    # flower
+    #brush_up()
+    #wash_brush()
+    #get_color(5)
+
+    wash_brush()
+    park()
+
+
+flower_scene()
