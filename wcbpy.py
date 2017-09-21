d8ea3203d73b7c32c673460426b588e41488a93d
jrheard <jrheard@cs.stanford.edu>
2017-09-20 18:17:15 -0700

    WIP on turtle integration, draws stuff to the screen but it has nothing to do with what we actually want to have drawn to the screen

--- wcbpy.py
+++ wcbpy.py
@@ -6,7 +6,13 @@ import turtle
 # Global mutable state. Forgive me.
 state = {
     'connected_to_bot': False,
+    'window': None,
+    'turtle': None,
 }
+
+# These measurements are in "steps", which are basically pixels.
+WCB_WIDTH = 620
+WCB_HEIGHT = 450
 
 def make_cnc_request(endpoint):
     if state['connected_to_bot']:
@@ -21,10 +27,15 @@ def initialize():
     except requests.exceptions.ConnectionError:
         state['connected_to_bot'] = False
 
+    state['window'] = turtle.Screen()
+    state['window'].setup(width=WCB_WIDTH, height=WCB_HEIGHT)
+    state['turtle'] = turtle.Turtle()
+    # TODO set turtle heading to scratch default angle
+
     brush_up()
     park()
-    # reset turtle
-    # TODO how to deal with scratch positions vs turtle positions? make a utility conversion function
+    # TODO how to deal with scratch positions vs turtle positions? make a utility conversion function? or do they have the same origin / coordinate system?
+    # i don't think they have the same coordinate system, the angles in scratch are shifted by -90 right?
 
 def cleanup():
     brush_up()
@@ -38,6 +49,7 @@ def wash_brush():
     make_cnc_request("pen.wash")
 
 def get_color(index):
+    # XXXX what to do for turtles?
     if index in range(0, 8):
         make_cnc_request("tool.color./" + str(index))
     else:
@@ -51,18 +63,24 @@ def brush_up():
 
 def move_to(x, y):
     make_cnc_request("coord/{0}/{1}".format(x, y))
+    state['turtle'].goto(x, y)
 
 def point_in_direction(angle):
     make_cnc_request("move.absturn./" + str(angle))
+    # XXXX will likely need to shift this to match scratch angles
+    state['turtle'].setheading(angle)
 
 def move_forward(num_steps):
     make_cnc_request("move.forward./" + str(num_steps))
+    state['turtle'].forward(num_steps)
 
 def turn_left(relative_angle):
     make_cnc_request("move.left./" + str(relative_angle))
+    state['turtle'].left(relative_angle)
 
 def turn_right(relative_angle):
     make_cnc_request("move.right./" + str(relative_angle))
+    state['turtle'].right(relative_angle)
 
 # Test program
 
